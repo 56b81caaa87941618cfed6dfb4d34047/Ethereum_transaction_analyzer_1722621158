bda3ccbc8a0fe3dda4cef92ab8b0fec39b4d522f
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-02 23:22:51 +0530

    Placeholder text replacement

--- components/simple_footer_component_1722621161.js
+++ components/simple_footer_component_1722621161.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722621161", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            EtherScan - Ethereum Transaction Analytics
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Stay ahead of the game with real-time monitoring and analysis of new Ethereum transactions. EtherScan provides comprehensive insights into the latest activity on the Ethereum blockchain.
                     </div>
                 </div>
             </div>
